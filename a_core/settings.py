0ddc68bbb1809bde887ef05d133f17ed496cf054
Aravinthishu <aravintharavinth7746@gmail.com>
2024-09-24 17:24:56 +0530

    update template

--- a_core/settings.py
+++ a_core/settings.py
@@ -97,7 +97,7 @@ ROOT_URLCONF = 'a_core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
